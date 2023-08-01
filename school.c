8a4a5e62c8a47edcf71336ea1a5ce290c3ba1169
TamariIv <taivgi@g.jct.ac.il>
2023-08-01 11:01:41 +0300

    add menu, insert, delete and search

--- school.c
+++ school.c
@@ -18,6 +18,31 @@ typedef struct Student {
     struct Student* next;
 } Student;
 
+enum menu_inputs {
+    Insert = '0',
+    Delete = '1',
+    Edit = '2',
+    Search = '3',
+    Showall = '4',
+    Top10 = '5',
+    UnderperformedStudents = '6',
+    Average = '7',
+    Export = '8',
+    Exit = '9'
+};
+
+enum courses {
+    Biology = 0,
+    Algebra = 1,
+    Chemistry = 2,
+    Calculus = 3,
+    English = 4,
+    History = 5,
+    Physics = 6,
+    Grammar = 7,
+    Geography = 8,
+    Statistics = 9
+};
 
 // Function to create a new student node
 Student* createStudentNode(const char* firstName, const char* lastName, const char* phoneNumber, int grade, int class, int marks[]) {
@@ -37,16 +62,134 @@ Student* createStudentNode(const char* firstName, const char* lastName, const ch
 }
 
 
-// Function to add a student to a class
+// Function to add a student to the school
 void addStudent(Student* school[NUM_GRADES][NUM_CLASSES], Student* newStudent) {
     if (school[newStudent->grade-1][newStudent->class-1] == NULL) {
+        // If no student in class - add as first student
         school[newStudent->grade-1][newStudent->class-1] = newStudent;
     } else {
+        // Add student to the head of the list
         Student* currentStudent = school[newStudent->grade-1][newStudent->class-1];
         school[newStudent->grade-1][newStudent->class-1] = newStudent;
         newStudent->next = currentStudent;
     }
 }
+
+
+// Function to find a student by first name and last name in the school system
+Student* findStudent(Student* school[NUM_GRADES][NUM_CLASSES], const char* firstName, const char* lastName) {
+    for (int grade = 0; grade < NUM_GRADES; ++grade) {
+        for (int classNum = 0; classNum < NUM_CLASSES; ++classNum) {
+            Student* currentStudent = school[grade][classNum];
+            while (currentStudent != NULL) {
+                if (strcmp(currentStudent->firstName, firstName) == 0 &&
+                    strcmp(currentStudent->lastName, lastName) == 0) {
+                    return currentStudent;
+                }
+                currentStudent = currentStudent->next;
+            }
+        }
+    }
+    return NULL; // Student not found
+}
+
+
+// Function to delete a student from the school system
+void deleteStudent(Student* school[NUM_GRADES][NUM_CLASSES]) {
+    char firstName[50], lastName[50];
+
+    printf("Enter the first name of the student to delete: ");
+    scanf("%49s", firstName);
+
+    printf("Enter the last name of the student to delete: ");
+    scanf("%49s", lastName);
+
+    // Find the student in the school system
+    Student* targetStudent = findStudent(school, firstName, lastName);
+
+    if (targetStudent == NULL) {
+        printf("Student not found.\n");
+        return;
+    }
+
+    printf("Found student:\n");
+    printf("Name: %s %s\n", targetStudent->firstName, targetStudent->lastName);
+    printf("Grade: %d\n", targetStudent->grade);
+    printf("Class: %d\n", targetStudent->class);
+    printf("Phone Number: %s\n", targetStudent->phoneNumber);
+    printf("\n");
+
+    // Ask the user to confirm the deletion
+    printf("Do you want to delete this student? (y/n): ");
+    char confirm;
+    scanf(" %c", &confirm);
+
+    if (confirm == 'y' || confirm == 'Y') {
+        // Delete the student
+        for (int grade = 0; grade < NUM_GRADES; ++grade) {
+            for (int classNum = 0; classNum < NUM_CLASSES; ++classNum) {
+                Student* currentStudent = school[grade][classNum];
+                Student* prevStudent = NULL;
+                while (currentStudent != NULL) {
+                    if (currentStudent == targetStudent) {
+                        if (prevStudent != NULL) {
+                            prevStudent->next = currentStudent->next;
+                        } else {
+                            school[grade][classNum] = currentStudent->next;
+                        }
+                        free(targetStudent);
+                        printf("Student deleted successfully.\n");
+                        return;
+                    }
+                    prevStudent = currentStudent;
+                    currentStudent = currentStudent->next;
+                }
+            }
+        }
+    } else {
+        printf("Deletion cancelled.\n");
+    }
+}
+
+
+// Function to get new student details from the user and perform validation
+void addNewStudent(Student* school[NUM_GRADES][NUM_CLASSES]) {
+    char firstName[50], lastName[50], phoneNumber[15];
+    int grade, classNum, marks[10];
+
+    printf("Enter the first name: ");
+    scanf("%49s", firstName);
+    printf("Enter the last name: ");
+    scanf("%49s", lastName);
+    printf("Enter the phone number: ");
+    scanf("%14s", phoneNumber);
+    printf("Enter the grade (1-12): ");
+    scanf("%d", &grade);
+    printf("Enter the class number (1-10): ");
+    scanf("%d", &classNum);
+
+    if (grade < 1 || grade > NUM_GRADES || classNum < 1 || classNum > NUM_CLASSES) {
+        printf("Error: Invalid grade or class number.\n");
+        return;
+    }
+
+    printf("Enter the marks for 10 subjects (separated by spaces): ");
+    for (int i = 0; i < 10; ++i) {
+        scanf("%d", &marks[i]);
+    }
+
+    // Clear the input buffer to consume any extra characters in the line
+    int c;
+    while ((c = getchar()) != EOF && c != '\n');
+
+    Student* newStudent = createStudentNode(firstName, lastName, phoneNumber, grade, classNum, marks);
+    if (newStudent == NULL) {
+        printf("Error: Memory allocation failed.\n");
+    } else {
+        addStudent(school, newStudent);
+    }
+}
+
 
 
 // Function to read students from a text file and add them to the school system
@@ -99,7 +242,7 @@ void readStudentsFromFile(const char* filename, Student* school[NUM_GRADES][NUM_
 }
 
 
-
+// Function to print all the students in the school
 void printAllStudents(Student* school[NUM_GRADES][NUM_CLASSES]) {
     for (int grade = 0; grade < NUM_GRADES; ++grade) {
         for (int classNum = 0; classNum < NUM_CLASSES; ++classNum) {
@@ -136,6 +279,97 @@ void freeStudents(Student* school[NUM_GRADES][NUM_CLASSES]) {
     }
 }
 
+// Function to search for a student in the school system
+void searchStudent(Student* school[NUM_GRADES][NUM_CLASSES]) {
+    char firstName[50], lastName[50];
+
+    printf("Enter the first name of the student to search: ");
+    scanf("%49s", firstName);
+
+    printf("Enter the last name of the student to search: ");
+    scanf("%49s", lastName);
+
+    // Find the student in the school system
+    Student* targetStudent = findStudent(school, firstName, lastName);
+
+    if (targetStudent == NULL) {
+        printf("Student not found.\n");
+    } else {
+        printf("Found student:\n");
+        printf("Name: %s %s\n", targetStudent->firstName, targetStudent->lastName);
+        printf("Grade: %d\n", targetStudent->grade);
+        printf("Class: %d\n", targetStudent->class);
+        printf("Phone Number: %s\n", targetStudent->phoneNumber);
+        printf("Marks: ");
+        for (int i = 0; i < 10; ++i) {
+            printf("%d ", targetStudent->marks[i]);
+        }
+        printf("\n");
+    }
+}
+
+void menu(Student* school[NUM_GRADES][NUM_CLASSES]) {
+    char input;
+    // school.name = "schoolName";
+    do {
+        printf("\n|School Manager<::>Home|\n");
+        printf("--------------------------------------------------------------------------------\n");
+        //printf("Welcome to ( %s ) School!\nYou have inserted ( %zu ) students.\n\n", school.name, school.numOfStudents);
+        printf("\t[0] |--> Insert\n");
+        printf("\t[1] |--> Delete\n");
+        printf("\t[2] |--> Edit\n");
+        printf("\t[3] |--> Search\n");
+        printf("\t[4] |--> Show All\n");
+        printf("\t[5] |--> Top 10 students per course\n");
+        printf("\t[6] |--> Underperformed students\n");
+        printf("\t[7] |--> Average per course\n");
+        printf("\t[8] |--> Export\n");
+        printf("\t[9] |--> Exit\n");
+        printf("\n\tPlease Enter Your Choice (0-9): ");
+        input = getc(stdin);
+        fflush(stdin);
+        getc(stdin);
+        switch (input) {
+            case Insert:
+                addNewStudent(school);
+                break;
+            case Delete:
+                deleteStudent(school);
+                break;
+            case Edit:
+                //editStudentGrade();
+                break;
+            case Search:
+                searchStudent(school);
+                break;
+            case Showall:
+                //printAllStudents();
+                break;
+            case Top10:
+                //printTopNStudentsPerCourse();
+                break;
+            case UnderperformedStudents:
+                //printUnderperformedStudents();
+                break;
+            case Average:
+                //printAverage();
+                break;
+            case Export:
+                //exportDatabase();
+                break;
+            case Exit:
+                //handleClosing();
+                break;
+            default:
+                printf("\nThere is no item with symbol \"%c\".Please enter a number between 1-10!\nPress any key to continue...",
+                       input);
+                getc(stdin);
+                getc(stdin);
+                break;
+        }
+    } while (input != Exit);
+}
+
 
 int main() {
     Student* school[NUM_GRADES][NUM_CLASSES] = {0};
@@ -143,7 +377,9 @@ int main() {
     const char* filename = "students_with_class.txt";
     readStudentsFromFile(filename, school);
 
-    printAllStudents(school);
+    // printAllStudents(school);
+    menu(school);
+
 
     freeStudents(school);
     return 0;
